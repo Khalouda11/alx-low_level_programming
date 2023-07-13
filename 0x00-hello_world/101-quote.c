2908754a3d621d6dff0b6b02e7761f3148e84d7d
khalid <Khalidmagdy845@gmail.com>
2023-07-13 16:56:14 -0700

    s

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,14 +3,15 @@
 /**
  * main - Entry point
  *
- * Description: a C program which uses the puts function to print
+ * Description: a C program which uses the char function to print
  *              a message to the standard output
  *
- * Return: Always 0 (Success)
- */
+ * Return: Always 1 (not Success)
+*/
+
 int main(void)
-}
+{
 char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(1, quo, 590);
 return (1);
-{
+}
