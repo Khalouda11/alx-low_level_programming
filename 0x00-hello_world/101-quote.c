2a4532490b2977ca7cae8932db2efd94111c5cf0
khalid <Khalidmagdy845@gmail.com>
2023-07-13 16:42:41 -0700

    s

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: a C program which uses the puts function to print
+ *              a message to the standard output
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+}
+char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+return (1);
+{
