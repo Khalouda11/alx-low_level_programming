ba41c0c0ed164513a8d6d63cfc48e849323dbca3
khalid <Khalidmagdy845@gmail.com>
2023-07-18 00:34:34 -0700

    a

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,19 @@
 #include "main.h"
 
 /**
- * print_alphabet - utilizes on the _putchar function
+ * print_alphabet - utilizes on the _putchar function will print x10
  *
 */
+
 void print_alphabet(void)
 {
-int ch;
+int n, ch;
+for (n = 1; n <= 10; n++)
+{
 for (ch = 'a'; ch <= 'z'; ch++)
 {
 _putchar(ch);
+}
 _putchar('\n');
 }
 }
