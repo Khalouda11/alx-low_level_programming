2db5ea611d17f912a3748001664e0a37c64cf717
khalid <Khalidmagdy845@gmail.com>
2023-07-24 00:30:00 -0700

    s

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,10 +8,10 @@
  */
 void rev_string(char *s)
 {
-int len;
+int len, i;
 for (len = 0; s[len] != '\0'; ++len)
 ;
-for (int i = 0; i < len / 2; i++)
+for (i = 0; i < len / 2; i++)
 {
 char temp = s[i];
 s[i] = s[len - i - 1];
