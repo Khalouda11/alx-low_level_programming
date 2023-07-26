e3e7f5e1ef8df29a52d638046279539e3a0f8b17
khalid <Khalidmagdy845@gmail.com>
2023-07-26 10:37:49 -0700

    s

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _strcmp - function that compares two strings
  *
@@ -7,6 +8,7 @@
  *
  * Return: 1 if true, 0 if false
 */
+
 int _strcmp(char *s1, char *s2)
 {
 int equal = 0;
@@ -17,8 +19,8 @@ if (*s1 != *s2)
 equal = ((int)*s1 - 48) - ((int)*s2 - 48);
 break;
 }
-s1++
-s2++
+s1++;
+s2++;
 }
 return (equal);
 }
