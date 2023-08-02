47bb38cd91624f4ea339f30a986cef38e6e11028
khalid <Khalidmagdy845@gmail.com>
2023-08-02 02:29:34 -0700

    s

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -4,6 +4,9 @@
 /**
  * last_index - returns the last index of a string
  * @s: pointer the string
+ * @start: starting index of the string to check
+ * @end: ending index of the string to check
+ * @mod: modulo value used to determine the middle index of the string
  * Return: int
  *
 */
@@ -37,15 +40,27 @@ return (check(s, 0, end - 1, end % 2));
  * @start: starting index of the string to check
  * @end: ending index of the string to check
  * @mod: modulo value used to determine the middle index of the string
- * Return: 0 or 1
-*/
+ *
+ * This function recursively checks whether the string `s` is a palindrome by
+ * comparing the characters at the `start` and `end` indices. The `mod`
+ * is used to determine the middle index of the string. If all characters are
+ * the same, the function returns 1. Otherwise, it returns 0.
+ *
+ * Return: 0 if string is not a palindrome, 1 if it is
+ */
 
 int check(char *s, int start, int end, int mod)
 {
 if ((start == end && mod != 0) || (start == end + 1 && mod == 0))
+{
 return (1);
+}
 else if (s[start] != s[end])
+{
 return (0);
+}
 else
+{
 return (check(s, start + 1, end - 1, mod));
+}
 }
