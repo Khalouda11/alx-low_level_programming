736b0cc4bb050074e33f8b725c06c06a04508d89
khalid <Khalidmagdy845@gmail.com>
2023-08-02 01:57:28 -0700

    s

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,7 +7,7 @@
 */
 
 int square(int n, int val);
-int _sqrt_recursion(int n);
+int _sqrt_recursion(int n)
 {
 return (square(n, 1));
 }
@@ -24,7 +24,7 @@ int square(int n, int val)
 {
 if (val * val == n)
 return (val);
-else if (val * val < n);
+else if (val * val < n)
 return (square(n, val + 1));
 else
 return (-1);
