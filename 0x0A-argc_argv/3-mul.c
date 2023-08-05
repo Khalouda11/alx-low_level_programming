1f8d838f99484ce086f214af71f6b60ee3ea7143
khalid <Khalidmagdy845@gmail.com>
2023-08-05 05:38:43 -0700

    s

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,12 +9,12 @@
  */
 
 
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
 int sum;
 if (argc == 3)
 {
-sum = atoi(argv[1] * atoi(argv[2]));
+  sum = atoi(argv[1]) * atoi(argv[2]);
 printf("%d\n", sum);
 }
 else
