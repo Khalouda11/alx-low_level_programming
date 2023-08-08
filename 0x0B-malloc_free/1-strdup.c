9029352299b0ede36d394dbbf474723006b24202
khalid <Khalidmagdy845@gmail.com>
2023-08-08 05:00:03 -0700

    s

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,7 +9,7 @@
 */
 char *_strdup(char *str)
 {
-int i =0, size = 0;
+int i = 0, size = 0;
 char *m;
 if (str == NULL)
 {
@@ -21,7 +21,7 @@ for (; str[size] != '\0'; size++)
 /*+1 on the size puts the end of string character*/
 m = malloc(size * sizeof(*str) + 1);
 }
-if (m ==0)
+if (m == 0)
 {
 return (NULL);
 }
