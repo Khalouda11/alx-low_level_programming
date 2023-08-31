04e6aca8e77b0ab4f280a698d77f7ed363163dbc
Khalouda11 <khalidmagdy845@gmail.com>
2023-08-31 05:28:34 -0700

    s

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,24 +5,17 @@
  * @n: the number to print
  *
  * Return: void
-*/
+ */
 void print_binary(unsigned long int n)
 {
-int bit = sizeof(n) * 8, printed = 0;
-while (bit)
-{
-if (n & !1L << --bit)
-{
-_putchar('1');
-printed++;
-}
-else if (printed)
+if (n == 0)
 {
 _putchar('0');
+return;
 }
-}
-if (!printed)
+if (n >> 1)
 {
-_putchar('0');
+print_binary(n >> 1);
 }
+_putchar((n & 1) + '0');
 }
